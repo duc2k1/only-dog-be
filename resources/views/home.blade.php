5e467d22a9d88eca3c0f4360dac49fc75afcb39e
duc622 <duc.phan622@gmail.com>
2021-09-22 18:31:33 +0700

    remove php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Social media</title>
+    <title>{{ env('APP_NAME') }}</title>
 </head>
+
 <body>
-    <h1>HELLO WORLD</h1>
+    <div style="text-align:center">
+        <h1>ALL API</h1>
+        <p>User: /user</p>
+        <p>Post: /post</p>
+        <p>Auth: /auth</p>
+    </div>
+
 </body>
+
 </html>
