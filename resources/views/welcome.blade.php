95db0288838b2c9bcd09ae91332f96926c2f776b
duc622 <duc.phan622@gmail.com>
2021-09-22 12:04:35 +0700

    update title web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Social media API</title>
+    <title>Social media</title>
 </head>
 <body>
     <h1>HELLO WORLD</h1>
