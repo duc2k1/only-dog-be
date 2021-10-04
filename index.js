872833ff7ab770433de7ec452569c12778765827
duc622 <duc.phan622@gmail.com>
2021-10-05 03:35:27 +0700

    update index

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("objsdfesct");
+const express = require("express");
+const app = express();
+const PORT = 6969 || prompt.env.PORT;
+app.get("/", (res, req) => {
+	req.send("<h1>ok</h1>");
+});
+app.listen(PORT);
