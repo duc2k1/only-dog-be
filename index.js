1377d8b1f7e7c04c4b15c84b3a943e8fef99dc24
duc622 <duc.phan622@gmail.com>
2021-10-05 09:17:05 +0700

    update listening port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 6969 || process.env.PORT;
+
 app.get("/", (res, req) => {
 	req.send("<h1>ok</h1>");
 });
-app.listen(PORT);
+app.listen(PORT, () => console.log("Listening port: " + PORT));
