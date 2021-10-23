74c2c63ccfe320e7b9b45f1c8b27829b00493590
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-10-23 09:43:13 +0700

    update user model

--- models/User.js
+++ models/User.js
@@ -6,23 +6,33 @@ const UserSchema = new Schema({
     type: String,
     required: true,
     unique: true,
+    min:3,
+    max:25,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    max50,
+    min:3,
   },
   password: {
     type: String,
     required: true,
+    min:6
   },
   pathAvatar: {
     type: String,
+  },
+  followers:{
+    type:Array,
+    default:[],
   },
   createdAt: {
     type: Date,
     default: Date.now,
   },
+  
 });
 //--------------------------------------------------------------
 export default mongoose.model("users", UserSchema);
