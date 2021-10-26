7bcd16e91b347ed6ea20dde6af220348a55c235f
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-10-26 17:03:35 +0700

    update feature/LikeUnlikePost

--- src/routes/post.js
+++ src/routes/post.js
@@ -61,4 +61,37 @@ router.post(
     res.send(file);
   }
 );
+//--------------------------------------------------------------
+router.put("/:id/like", verifyToken, async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id);
+  
+    if(!post.likes.includes(req.body.userId))
+    {
+      await post.updateOne({$push:{likes:req.body.userId}});
+      res.status(200).json("The post was liked");
+    }else{
+      await post.updateOne({$pull:{likes:req.body.userId}});
+      res.status(200).json("The post was unliked");
+    }
+  }catch (err){
+    res.status(500).json(err);
+  }
+});
+//--------------------------------------------------------------
+router.put("/:id/dislike", verifyToken, async (req, res) => {
+  try{
+    const post = await Post.findById(req.params.id);
+    if(!post.dislikes.includes(req.body.userId))
+    {
+      await post.updateOne({$push:{dislikes:req.body.userId}});
+      res.status(200).json("The post was disliked");
+    }else{
+      await post.updateOne({$pull:{dislikes:req.body.userId}});
+      res.status(200).json("The post was undisliked");
+    }
+  }catch (err){
+    res.status(500).json(err);
+  }
+});
 export default router;
