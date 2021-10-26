f16086746674a72d2f14907b76e3a602fc3ddd55
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-10-26 14:49:53 +0700

    completed feature follow user (be)

--- src/routes/user.js
+++ src/routes/user.js
@@ -30,9 +30,36 @@ router.get("/find_one", async (req, res) => {
     res.status(404).json({ success: false, message: "Not found user" });
   }
 });
-
 //--------------------------------------------------------------
+//user_id: user current (userId) => send from body
+//user_id_follow: user has followed by another one
 
+router.put("/follow", async (req, res) => {
+  const user_id = req.body.userId;
+  const user_id_follow = req.query.user_id_follow;
+  if (user_id_follow) {
+    if (req.body.userId !== req.query.user_id_follow) {
+      try {
+        const user = await User.findById(user_id_follow);
+        const currentUser = await User.findById(user_id);
+        //check followers list
+        if (!user.followers.includes(user_id)) {
+          await user.updateOne({ $push: { followers: user_id } });
+          await currentUser.updateOne({ $push: { following: user_id_follow } });
+          res.status(200).json("User was followed");
+        } else {
+          res.status(403).json("You allready follow this user");
+        }
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can't follow yourself");
+    }
+  } else {
+    res.status(404).json({ success: false, message: "Not found user" });
+  }
+});
 //--------------------------------------------------------------
 
 export default router;
