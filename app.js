74e7a3cf3ac579b103485b547ee30bf1424e40d1
vinhvvdvGithub <vinhvvdv@gmail.com>
2021-10-28 00:27:27 +0700

    update handle refreshToken

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import postRouter from "./src/routes/post.js";
 import userRouter from "./src/routes/user.js";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import redis from "redis";
 //--------------------------------------------------------------
 const app = express();
 const PORT = process.env.PORT || 5500;
@@ -31,5 +32,28 @@ app.use(cors());
 app.use("/auth", authRouter);
 app.use("/posts", postRouter);
 app.use("/users", userRouter);
+//--------------------------------------------------------------
+//cors
+app.use(cors());
+//redis
+const redisClient = redis.createClient({
+  host: "apn1-brave-oryx-30883.upstash.io",
+  port: 30883,
+  password: "88104c06b1244bfa9e60616f3e12897b",
+});
+redisClient.on("ready", function() {  
+  console.log("Connected to Redis server successfully");  
+});
+const set = (key, value) => {
+  redisClient.set(key, JSON.stringify(value));
+};
+const get = (req, res, next) => {
+  let key = req.route.path;
+  redisClient.get(key, (error, data) => {
+    if (error) res.status(400).send(err);
+    if (data !== null) {
+      res.status(200).send(JSON.parse(data));
+    } else next();
+  });
 //--------------------------------------------------------------
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
