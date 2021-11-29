8647735112ca81701f64ecf24da75c776941b795
tranthuong021001 <tranthuong021001@gmail.com>
2021-11-29 20:41:54 +0700

    complete feature delete comment

--- src/models/Post.js
+++ src/models/Post.js
@@ -20,6 +20,19 @@ const PostSchema = new Schema({
     type: Array,
     default: [],
   },
+  comments : [{
+    content :{
+      type: String
+    },
+    by:{
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    },
+    date:{
+      type: Date,
+      default: Date.now
+    }
+  },{ optimisticConcurrency: true, versionKey: 'version' }],
   createdAt: {
     type: Date,
     default: Date.now,
