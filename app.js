6b3836cae2034a0ab66d7b903813567363292d46
duc <duc.phan622@gmail.com>
2021-12-30 09:52:23 +0700

    add https

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import fs from "fs";
+import https from "https";
 import connectToMongoDb from "./src/connects/connectToMongoDb.js";
-import { redisStatus } from "./src/connects/connectToRedis.js";
+import {
+  redisStatus
+} from "./src/connects/connectToRedis.js";
 import authRouter from "./src/routes/auth.js";
 import postRouter from "./src/routes/post.js";
 import userRouter from "./src/routes/user.js";
@@ -33,4 +37,13 @@ app.get("/*", (req, res) => {
   res.status(404).send("<h1>404</h1>");
 });
 //--------------------------------------------------------------
-app.listen(PORT, () => console.log(`✅ Server: http://localhost:${PORT}`));
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
+
+sslServer.listen(PORT, () =>
+  console.log(`✅ Server: https://localhost:${PORT}`)
+);
